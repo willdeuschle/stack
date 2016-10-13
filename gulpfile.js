2e9808fa1bc722ed141f1504fc3193cac30764e1
willdeuschle <wjdeuschle@gmail.com>
2016-10-13 08:15:46 -0400

    practicing stack

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('build', () => {
+    return gulp.src(['src/**/*.js'])
+        .pipe(babel())
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('default', ['build']);
